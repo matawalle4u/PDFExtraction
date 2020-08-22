7648dda2414229785c5224e44cc73860fa4c02fd
Adam Mustapha <matawalle4u@gmail.com>
2020-08-21 23:45:35 -0700

    Citation Extraction added

--- extr_.py
+++ extr_.py
@@ -1,4 +1,4 @@
-import json, os, subprocess, sys
+import json, os, subprocess, sys,re
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
 """
@@ -49,11 +49,12 @@ class PDFExtract:
         #self.dump_ext = dump_ext
         self.file_ext = file_ext
         self.pdf_files = self.get_files_by_ext()
+        self.citations = []
 
         #Page range below parsed as command line args
-        self.start_page = int(sys.argv[1])
-        self.end_page = int(sys.argv[2])
-        self.result = open('result.{}'.format(sys.argv[3]), 'w')
+        self.start_page = int(sys.argv[2])
+        self.end_page = int(sys.argv[3])
+        self.result = open('result.{}'.format(sys.argv[4]), 'w')
 
         for pdf_file in self.pdf_files:
                 self.readers.append(PdfFileReader(pdf_file, 'r'))
@@ -80,6 +81,18 @@ class PDFExtract:
     # def intall_libry(self, tool_name):
     #     subprocess.check_call([sys.executable, "-m", "pip", "install", tool_name])
 
+    def get_citations(self, text):
+
+        author = "(?:[A-Z][A-Za-z'`-]+)"
+        etal = "(?:et al.?)"
+        additional = "(?:,? (?:(?:and |& )?" + author + "|" + etal + "))"
+        year_num = "(?:19|20)[0-9][0-9]"
+        page_num = "(?:, p.? [0-9]+)?"  # Always optional
+        year = "(?:, *"+year_num+page_num+"| *\("+year_num+page_num+"\))"
+        regex = "(" + author + additional+"*" + year + ")"
+        matches = re.findall(regex, text)
+        return matches
+
     def cire_non_ascii(self, string):
         ''' Returns the string without non ASCII characters'''
         stripped = (c for c in string if 0 < ord(c) < 127)
@@ -100,14 +113,32 @@ class PDFExtract:
             for i in range(self.start_page-1, self.end_page):
                 try:
                     pageText = self.readers[0].getPage(i).extractText().split()
+                    
                     for text in pageText:
                         text = self.cire_non_ascii(text)
                         try:
-                            self.result.write('{}\n'.format(text))
+                            if sys.argv[1]=='t':
+                                self.result.write('{}\n'.format(text))
+                            elif sys.argv[1]=='c':
+                                if self.get_citations(text):
+                                    cited = self.get_citations(text)
+                                    
+                                    for i in cited:
+                                        if i not in self.citations:
+                                            self.result.write('{}\n'.format(i))
+                                            self.citations.append(i)
+                                        
+                            elif sys.argv[1]=='u':
+                                pass
+                            else:
+                                pass
+
+                                #print(self.get_citations(text))
                         except UnicodeEncodeError:
                             continue
                 except IndexError:
                     continue
+            print(self.citations)
         else:
             print('Provide One PDF file at a time')
 
